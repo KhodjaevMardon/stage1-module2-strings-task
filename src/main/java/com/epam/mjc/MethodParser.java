d146b3d899cceb3010ea493e586dddce68e15c06
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-19 22:26:11 +0500

    finished the task

--- src/main/java/com/epam/mjc/MethodParser.java
+++ src/main/java/com/epam/mjc/MethodParser.java
@@ -1,25 +1,56 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MethodParser {
 
     /**
      * Parses string that represents a method signature and stores all it's members into a {@link MethodSignature} object.
      * signatureString is a java-like method signature with following parts:
-     *      1. access modifier - optional, followed by space: ' '
-     *      2. return type - followed by space: ' '
-     *      3. method name
-     *      4. arguments - surrounded with braces: '()' and separated by commas: ','
+     * 1. access modifier - optional, followed by space: ' '
+     * 2. return type - followed by space: ' '
+     * 3. method name
+     * 4. arguments - surrounded with braces: '()' and separated by commas: ','
+     *
      * Each argument consists of argument type and argument name, separated by space: ' '.
      * Examples:
-     *      accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
-     *      private void log(String value)
-     *      Vector3 distort(int x, int y, int z, float magnitude)
-     *      public DateTime getCurrentDateTime()
+     * accessModifier returnType methodName(argumentType1 argumentName1, argumentType2 argumentName2)
+     * private void log(String value)
+     * Vector3 distort(int x, int y, int z, float magnitude)
+     * public DateTime getCurrentDateTime()
      *
      * @param signatureString source string to parse
      * @return {@link MethodSignature} object filled with parsed values from source string
      */
     public MethodSignature parseFunction(String signatureString) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        String[] splitByDirection = signatureString.split("[()]");
+
+        String[] splitBySpace = splitByDirection[0].split(" ");
+        String
+                accessModifier = splitBySpace.length == 3 ? splitBySpace[0] : null,
+                returnType = splitBySpace[splitBySpace.length - 2],
+                methodName = splitBySpace[splitBySpace.length - 1];
+
+        List<MethodSignature.Argument> args =
+                splitByDirection.length > 1
+                        ? extractArguments(splitByDirection[1])
+                        : new ArrayList<>();
+
+        MethodSignature ans = new MethodSignature(methodName, args);
+        ans.setReturnType(returnType);
+        ans.setAccessModifier(accessModifier);
+
+        return ans;
+    }
+
+    private List<MethodSignature.Argument> extractArguments(String argumentsStr) {
+        String[] arguments = argumentsStr.split(",");
+        List<MethodSignature.Argument> ans = new ArrayList<>();
+        for (String arg : arguments) {
+            String[] splitBySpace = arg.trim().split(" ");
+            ans.add(new MethodSignature.Argument(splitBySpace[0], splitBySpace[1]));
+        }
+        return ans;
     }
 }
--- src/main/java/com/epam/mjc/StringSplitter.java
+++ src/main/java/com/epam/mjc/StringSplitter.java
@@ -1,5 +1,6 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -13,6 +14,34 @@ public class StringSplitter {
      * @return List of substrings
      */
     public List<String> splitByDelimiters(String source, Collection<String> delimiters) {
-        throw new UnsupportedOperationException("You should implement this method.");
+        List<String> ans = new ArrayList<>();
+        int startingIndex = 0;
+        for (int i = 0; i < source.length(); i++) {
+            for (String str : delimiters) {
+                if (checkWhetherToSplit(source, str, i)) {
+                    if (startingIndex < i) {
+                        ans.add(source.substring(startingIndex, i));
+                    }
+                    startingIndex = i + str.length();
+                    i = startingIndex - 1;
+                    break;
+                }
+            }
+        }
+        if (startingIndex < source.length()) {
+            ans.add(source.substring(startingIndex));
+        }
+
+        return ans;
+    }
+
+    private boolean checkWhetherToSplit(String source, String delimiter, int startingIndex) {
+        if (startingIndex < 0 || startingIndex >= source.length()) {
+            throw new StringIndexOutOfBoundsException("you cant cut like this");
+        }
+
+        if (startingIndex + delimiter.length() > source.length()) return false;
+
+        return source.startsWith(delimiter, startingIndex);
     }
 }
